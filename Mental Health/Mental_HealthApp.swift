570226ffe546d2714e8902e2d9a0047a756767bd
DoryMarlenovna <kalmakhanbetdariga@icloud.com>
2024-11-15 21:59:50 +0500

    Initial Commit

---
+++ Mental Health/Mental_HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mental_HealthApp.swift
+//  Mental Health
+//
+//  Created by Kalmakhanbet Dariga on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Mental_HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
