cc842337db6de3625628f7ffdccbc4bf305c5552
DoryMarlenovna <kalmakhanbetdariga@icloud.com>
2024-11-23 01:25:00 +0500

    tupoi

--- Mental Health/Constant/Mental_HealthApp.swift
+++ Mental Health/Constant/Mental_HealthApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Mental_HealthApp: App {
     var body: some Scene {
         WindowGroup {
-            SplashScreenView()
+            BooksView()
         }
     }
 }
--- Mental Health/View/OnboardingView.swift
+++ Mental Health/View/OnboardingView.swift
@@ -14,10 +14,42 @@ struct OnboardingView: View {
                 .frame(maxWidth:.infinity,alignment: .trailing)
                 .padding()
             Spacer()
+            
+                Text("Welcome ")
+                    .foregroundColor(Color("green"))
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                +
+                Text("to Calmind")
+                    .foregroundColor(.black)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+            HStack{
+                Image("onboarding")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: 255, maxHeight: 177)
+                    .padding(.bottom, 90)
+                
+                Image("onboard")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: 255, maxHeight: 177)
+                    .padding(.bottom, 90)
+                
+                Image("onboa")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: 255, maxHeight: 177)
+                    .padding(.bottom, 90)
+                
+            }
+        
         }
-      
     }
-}
+    }
+    
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
